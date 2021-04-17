e9fd4b290297d4ebf38dd1bc155eba5887af3e5c
Angel Lucero <angel.lucero.est@tecazuay.edu.ec>
2021-04-16 21:08:39 -0500

    Consumo de las categoriasdesde Firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public categorieslist = [];
   user = "Mi Cuenta"
   nombrePlatillo = 'Cuy';
   restaurante = 'El Alcatraz';
   precio = '$20.00'
-  constructor() { }
+
+  constructor(private toastservice: ToastService, private fbstore: AngularFirestore) {
+    this.getCategories()
+  }
+
+  async getCategories(){
+    try{
+      await this.fbstore.collection("categories").snapshotChanges()
+      .subscribe(data => {
+        console.log(data);
+        this.categorieslist = data.map(result => {
+          //console.log(result)
+          
+          return {
+            docid: result.payload.doc.id,
+            category_name: result.payload.doc.data()["category_name"],
+            category_image: result.payload.doc.data()["category_image"]
+            
+            
+          }
+        });
+      });console.log();
+    }catch(error){
+      this.toastservice.showToast(error.message, 2000);
+    }
+  }
 
 }
