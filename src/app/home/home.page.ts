4163648920d081910d22ca04044b488a4ebf8785
Angel Lucero <angel.lucero.est@tecazuay.edu.ec>
2021-04-16 23:20:56 -0500

    Diseño de la página Home, consumo de categorias y comida

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,9 @@ import { ToastService } from '../services/toast.service';
 })
 export class HomePage {
   public categorieslist = [];
+  public foodlist = [];
+
+
   user = "Mi Cuenta"
   nombrePlatillo = 'Cuy';
   restaurante = 'El Alcatraz';
@@ -16,28 +19,43 @@ export class HomePage {
 
   constructor(private toastservice: ToastService, private fbstore: AngularFirestore) {
     this.getCategories()
+    this.getFood();
   }
 
   async getCategories(){
     try{
       await this.fbstore.collection("categories").snapshotChanges()
       .subscribe(data => {
-        console.log(data);
         this.categorieslist = data.map(result => {
-          //console.log(result)
-          
           return {
             docid: result.payload.doc.id,
             category_name: result.payload.doc.data()["category_name"],
             category_image: result.payload.doc.data()["category_image"]
-            
-            
           }
         });
-      });console.log();
+      });
     }catch(error){
       this.toastservice.showToast(error.message, 2000);
     }
   }
 
+  async getFood(){
+    try{
+      await this.fbstore.collection("food").snapshotChanges()
+      .subscribe(data => {
+        this.foodlist = data.map(result => {
+          return {
+            docid: result.payload.doc.id,
+            food_name: result.payload.doc.data()["food_name"],
+            food_image: result.payload.doc.data()["food_image"],
+            food_restaurant: result.payload.doc.data()["food_restaurant"],
+            food_description: result.payload.doc.data()["food_description"],
+            food_cost: result.payload.doc.data()["food_cost"]
+          }
+        });
+      })
+    }catch(error){
+      this.toastservice.showToast(error.message, 2000);
+    }
+  }
 }
